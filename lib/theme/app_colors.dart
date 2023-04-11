b58805bd76d1781bce1a15db1f3ae65b913196a1
Artem Shylo <artem.shylo@proarea.com>
2023-04-11 19:24:06 +0300

    Theme

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors{
+  static const mainColor = Color(0xff93a9f5);
+
+  static const secondaryColor = Color(0xffb7c8fd);
+}
---
+++ lib/theme/app_text_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const mainTextStyle = TextStyle(fontSize: 32);
+  
+  static const secondaryTextStyle = TextStyle(fontSize: 20);
+}
